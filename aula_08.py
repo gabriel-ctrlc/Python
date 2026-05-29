78dc6c7a709d621a2457f4a962eb399fb20b0f7d
Gabriel Moura de Albuquerque <gsbriel.12.moura31@gmail.com>
2026-05-28 23:32:16 -0300

    bibliotecas, introdução

---
+++ aula_08.py
@@ -0,0 +1,18 @@
+# math       #import math    #from math import sqrt, ceil
+# ceil    math.ceil arredonda para cima
+# floor   math.floor arredond apara baixo
+# trunc
+# pow
+# sqrt   calcula raiz quadrada mais facil
+# factorial
+# Para estudar bibliotecas https://docs.python.org/3.13/library/index.html
+
+
+# from math import sqrt, floor, ceil
+# num = int(input('Digite um numero: '))
+# raiz = math.sqrt(num)
+# print('A raiz de {} é igual a {:.2f}'.format(num, math.floor(raiz)))
+
+import random
+num1 = random.random()
+print(num1)
