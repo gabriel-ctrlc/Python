b641674408754cc90b3dfd7f3aab354ae625c480
Gabriel Moura de Albuquerque <gsbriel.12.moura31@gmail.com>
2026-08-10 10:41:19 -0300

    adição dos desafios

---
+++ desafio26.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia uma frase pelo teclado e mostra:
+ # Quantas vezes aparece a letra"a"
+ # Em qual posição ela aparece a primeira vez
+ # Em que posição ela aprece a ultima vez.
---
+++ desafio27.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente
+ # Ex: Ana maria de Souza
+ # primeiro = Ana
+ # Ultimo = Souza
