74b952e43d4cde254cd0bb2d4ee6106d5aca173d
Gabriel Moura de Albuquerque <gsbriel.12.moura31@gmail.com>
2026-08-11 16:12:29 -0300

    finalizei o exercico 31

--- Curso em video - python/desafio31.py
+++ Curso em video - python/desafio31.py
@@ -1,2 +1,12 @@
 # Faça um programa que pergunte a distancia em Km. 
 # Calcule o preço da passagem. Cobrando R$0.50 por km para viagens de até 200km e R$0,45 para viagens mais longas.
+
+distancia = float(input('Digite quantos KM voce ira viajar: '))
+ 
+
+if distancia >= 200:
+    preco = distancia * 0.45
+else:
+    preco = distancia * 0.50
+
+print(f'O preço da passagem será de: {preco:.2f}')
