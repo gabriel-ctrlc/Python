395ffa4ffcd1dd1ef97626a3715e45bdf4d4277e
Gabriel Moura de Albuquerque <gsbriel.12.moura31@gmail.com>
2026-08-12 10:29:37 -0300

    adicionei as viariaveis do exercicio 33

--- Curso em video - python/desafio33.py
+++ Curso em video - python/desafio33.py
@@ -1,2 +1,5 @@
 # Faça um programa que leia tres numeros e mostre qual é maior e qual o menor
-variavel = input('Digite um valor')
+valor1 = input('Digite o primeiro valor: ')
+valor2 = input('Digite o segundo valor: ')
+valor3 = input('Digite o terceiro valor: ')
+
