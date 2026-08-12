2705714deca28a670589e060e18da66a6e9205a8
Gabriel Moura de Albuquerque <gsbriel.12.moura31@gmail.com>
2026-08-12 07:43:49 -0300

    conclusão do desafio 32

--- Curso em video - python/desafio32.py
+++ Curso em video - python/desafio32.py
@@ -1,1 +1,11 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO
+from datetime import date
+
+ano = int(input('Digite um Ano que voce quer analisar, ou digite 0 para selecionar o ano atual: '))
+
+if ano == 0:
+    ano = date.today().year
+if ano % 400 == 0:
+    print('O Ano que voce digitou é Bissexto')
+else:
+    print('O Ano que voce digitou não é Bissexto')
