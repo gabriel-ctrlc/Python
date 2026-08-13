860e74fd489e03a53d12df8b13db974aa776ccb1
Gabriel Moura de Albuquerque <gsbriel.12.moura31@gmail.com>
2026-08-13 08:28:55 -0300

    tentativa de inserção do if

--- Curso em video - python/desafio33.py
+++ Curso em video - python/desafio33.py
@@ -1,5 +1,11 @@
 # Faça um programa que leia tres numeros e mostre qual é maior e qual o menor
-valor1 = input('Digite o primeiro valor: ')
-valor2 = input('Digite o segundo valor: ')
-valor3 = input('Digite o terceiro valor: ')
+valor1 = float(input('Digite o primeiro valor: '))
+valor2 = float(input('Digite o segundo valor: '))
+valor3 = float(input('Digite o terceiro valor: '))
 
+if valor1 > valor2 and valor3:
+    print('O Priemiro numero digitado é o de maior valor')
+if valor2 > valor1 and valor3:
+    print('O segundo numero digitado é o de maior valor')
+if valor3 > valor1 and valor2:
+    print('O terceiro numero é o de maior valor')
