643211be4f8284dea3abccf178ebf10250c6bda5
Gabriel Moura de Albuquerque <gsbriel.12.moura31@gmail.com>
2026-08-18 12:18:07 -0300

    resolução do execicio 35 forma triangulo

--- Curso em video - python/desafio35.py
+++ Curso em video - python/desafio35.py
@@ -1,2 +1,13 @@
 # Desenvolva um porgrama que leia o comprimento de tres retas e diga ao usuario se elas podem ou não formar umm triangulo
+print("""-=-=-=-=-=-=-=-=-=-=-=-=
+Analisador de Triangulos
+-=-=-=-=-=-=-=-=-=-=-=-=""")
 
+a = float(input('Primeiro segmento: '))
+b = float(input('Segundo segmento: '))
+c = float(input('Terceiro segmento: '))
+
+if a + b > c and a + c > b and b + c > a:
+    print('Os segmentos acima PODEM FORMAR triângulo!')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo')
