5527d25f0944e11cdfe2ca3c3fb52b6d184e146b
Gabriel Moura de Albuquerque <gsbriel.12.moura31@gmail.com>
2026-08-18 10:52:47 -0300

    resolução do desafio 33

--- Curso em video - python/desafio33.py
+++ Curso em video - python/desafio33.py
@@ -1,11 +1,20 @@
 # Faça um programa que leia tres numeros e mostre qual é maior e qual o menor
-valor1 = float(input('Digite o primeiro valor: '))
-valor2 = float(input('Digite o segundo valor: '))
-valor3 = float(input('Digite o terceiro valor: '))
+a = int(input('Digite o primeiro valor: '))
+b = int(input('Digite o segundo valor: '))
+c = int(input('Digite o terceiro valor: '))
 
-if valor1 > valor2 and valor3:
-    print('O Priemiro numero digitado é o de maior valor')
-if valor2 > valor1 and valor3:
-    print('O segundo numero digitado é o de maior valor')
-if valor3 > valor1 and valor2:
-    print('O terceiro numero é o de maior valor')
+# Verificando quem é menor
+menor = a
+if b<a and b<c:
+    menor = b
+if c<a and c<b:
+    menor = c
+
+# Verificando quem é o maior
+maior = a
+if b>a and b>c:
+    maior  = b
+if c>a and c>b:
+    maior = c
+print('O menor valor digitado foi {}'.format(menor))
+print('O maio valor digitado foi {}'.format(maior))
